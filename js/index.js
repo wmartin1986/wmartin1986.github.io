19345001bc8cc1dc1ad85ea049faf1738343abf2
Will Martin <w_martin@outlook.com>
2019-02-13 00:55:57 -0700

    fixing js to loop every 5s

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 // envelope animation
+var firstRun = true;
 window.onload = function(){
   var tl = new TimelineLite({delay: 1}),
     firstBg = document.querySelectorAll('.text__first-bg'),
     secBg = document.querySelectorAll('.text__second-bg'),
     word  = document.querySelectorAll('.text__word');
-  
-  tl
-    .to(firstBg, 0.2, {scaleX:1})
-    .to(secBg, 0.2, {scaleX:1})
-    .to(word, 0.1, {opacity:1}, "-=0.1")  
-    .to(firstBg, 0.2, {scaleX:0})
-    .to(secBg, 0.2, {scaleX:0});
+	
+	function animate() {
+	if(firstRun){firstRun = false;}
+	else{
+		tl
+		.to(word, 0.1, {opacity:0}, "-=0.1")  
+	}
+	tl
+	.to(firstBg, 0.2, {scaleX:1})
+	.to(secBg, 0.2, {scaleX:1})
+	.to(word, 0.1, {opacity:1}, "-=0.1")  
+	.to(firstBg, 0.2, {scaleX:0})
+	.to(secBg, 0.2, {scaleX:0});
 
+	setTimeout(animate,6000);
+	}
+	animate();
 }
